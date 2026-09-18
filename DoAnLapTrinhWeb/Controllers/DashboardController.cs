30f1a2c631aaac0450232bd9b91c13678939fa45
KTeam-work <nguyenki.2711@gmail.com>
2026-09-18 11:20:45 +0700

    Tạo giao diện shift

--- DoAnLapTrinhWeb/Controllers/DashboardController.cs
+++ DoAnLapTrinhWeb/Controllers/DashboardController.cs
@@ -6,12 +6,12 @@ using System.Web.Mvc;
 
 namespace DoAnLapTrinhWeb.Controllers
 {
-    public class HomeController : Controller
+    public class DashboardController : Controller
     {
+        // GET: Dashboard
         public ActionResult Index()
         {
             return View();
         }
-
     }
 }
--- DoAnLapTrinhWeb/Controllers/ShiftController.cs
+++ DoAnLapTrinhWeb/Controllers/ShiftController.cs
@@ -6,12 +6,17 @@ using System.Web.Mvc;
 
 namespace DoAnLapTrinhWeb.Controllers
 {
-    public class HomeController : Controller
+    public class ShiftController : Controller
     {
+        // GET: Shift
         public ActionResult Index()
         {
             return View();
         }
 
+        public ActionResult Schedule()
+        {
+            return View();
+        }
     }
 }
