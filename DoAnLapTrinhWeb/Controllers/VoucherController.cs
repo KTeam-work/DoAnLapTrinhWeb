d3c5cd68e3bd70bd2ac6402303e8f18eb779315c
KTeam-work <nguyenki.2711@gmail.com>
2026-09-18 11:20:45 +0700

    Tạo giao diện trang chủ

--- DoAnLapTrinhWeb/Controllers/VoucherController.cs
+++ DoAnLapTrinhWeb/Controllers/VoucherController.cs
@@ -6,12 +6,12 @@ using System.Web.Mvc;
 
 namespace DoAnLapTrinhWeb.Controllers
 {
-    public class HomeController : Controller
+    public class VoucherController : Controller
     {
+        // GET: Voucher
         public ActionResult Index()
         {
             return View();
         }
-
     }
 }
