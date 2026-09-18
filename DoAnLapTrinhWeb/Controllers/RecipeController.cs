3047accd900e2f58aaee81c54bf25e1044d73120
KTeam-work <nguyenki.2711@gmail.com>
2026-09-18 11:20:45 +0700

    Push RecipeTạo giao diện Ingredient

--- DoAnLapTrinhWeb/Controllers/RecipeController.cs
+++ DoAnLapTrinhWeb/Controllers/RecipeController.cs
@@ -6,12 +6,12 @@ using System.Web.Mvc;
 
 namespace DoAnLapTrinhWeb.Controllers
 {
-    public class HomeController : Controller
+    public class RecipeController : Controller
     {
+        // GET: Recipe
         public ActionResult Index()
         {
             return View();
         }
-
     }
 }
